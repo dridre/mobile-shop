4daa4a7cfe3c637251fda425103e313626214237
Dridre <rubenborque@hotmail.com>
2025-04-23 22:19:37 +0200

    fix header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppBar, Toolbar, Typography, Badge, IconButton, Box } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { Link, useLocation } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import CartDropdown from '../cart/CartDropdown';
+import { fetchProductById } from '../../redux/slices/productSlice';
 
 const Header = () => {
     const cartItems = useSelector(state => state.cart.items);
     const location = useLocation();
+    const dispatch = useDispatch();
+
+    // Change from currentProduct to selectedProduct to match your Redux state
+    const selectedProduct = useSelector(state => state.products.selectedProduct);
 
     const [anchorEl, setAnchorEl] = useState(null);
     const open = Boolean(anchorEl);
+
+    useEffect(() => {
+        const paths = location.pathname.split('/').filter(p => p);
+        if (paths[0] === 'product' && paths.length === 2) {
+            const productId = paths[1];
+            dispatch(fetchProductById(productId));
+        }
+    }, [location.pathname, dispatch]);
 
     const handleCartClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -26,12 +39,16 @@ const Header = () => {
         if (paths.length === 0) {
             return 'Inicio';
         } else if (paths[0] === 'product' && paths.length === 2) {
+            const productName = selectedProduct ?
+                `${selectedProduct.brand} ${selectedProduct.model}` :
+                'Cargando...';
+
             return (
                 <>
                     <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>
                         Inicio
                     </Link>
-                    {' > Detalle del producto'}
+                    {` > ${productName}`}
                 </>
             );
         }
