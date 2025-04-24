2ab42ac9d7778853da4ce0bf2eb582ad7a8d1ab0
Dridre <rubenborque@hotmail.com>
2025-04-24 12:56:32 +0200

    size searchbar

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Paper, InputBase, IconButton } from '@mui/material';
+import { Paper, InputBase, IconButton, Box, useTheme, useMediaQuery } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { useDispatch } from 'react-redux';
 import { setSearchTerm } from '../../redux/slices/productSlice';
@@ -7,6 +7,8 @@ import { setSearchTerm } from '../../redux/slices/productSlice';
 const SearchBar = () => {
     const dispatch = useDispatch();
     const [localSearchTerm, setLocalSearchTerm] = React.useState('');
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
     const handleSearch = (e) => {
         const value = e.target.value;
@@ -15,29 +17,38 @@ const SearchBar = () => {
     };
 
     return (
-        <Paper
-            component="form"
+        <Box
             sx={{
-                p: '2px 4px',
                 display: 'flex',
-                alignItems: 'center',
+                justifyContent: isMobile ? 'center' : 'flex-end',
                 width: '100%',
+                mt: 3,
                 mb: 3
             }}
-            elevation={2}
-            onSubmit={(e) => e.preventDefault()}
         >
-            <InputBase
-                sx={{ ml: 1, flex: 1 }}
-                placeholder="Buscar por marca o modelo..."
-                inputProps={{ 'aria-label': 'buscar productos' }}
-                value={localSearchTerm}
-                onChange={handleSearch}
-            />
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                <SearchIcon />
-            </IconButton>
-        </Paper>
+            <Paper
+                component="form"
+                sx={{
+                    p: '2px 4px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    width: isMobile ? '100%' : '50%',
+                }}
+                elevation={2}
+                onSubmit={(e) => e.preventDefault()}
+            >
+                <InputBase
+                    sx={{ ml: 1, flex: 1 }}
+                    placeholder="Buscar por marca o modelo..."
+                    inputProps={{ 'aria-label': 'buscar productos' }}
+                    value={localSearchTerm}
+                    onChange={handleSearch}
+                />
+                <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
+        </Box>
     );
 };
 
